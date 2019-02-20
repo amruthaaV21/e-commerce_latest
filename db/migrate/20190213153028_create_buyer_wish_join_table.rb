c480e0623b3d4b9369b9d614db8106e2091fc231
amruthaaV21 <33821089+amruthaaV21@users.noreply.github.com>
2019-02-20 12:14:00 +0530

    Delete 20190213153028_create_buyer_wish_join_table.rb

--- db/migrate/20190213153028_create_buyer_wish_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateBuyerWishJoinTable < ActiveRecord::Migration[5.1]
-  def change
-    create_join_table :buyers, :sellers do |t|
-    t.index :buyer_id
-    t.index :seller_id
-    end
-  end
-end
