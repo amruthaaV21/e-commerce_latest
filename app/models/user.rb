4fc3924919844885714957a92e5868a42163195e
amruthaaV21 <33821089+amruthaaV21@users.noreply.github.com>
2019-02-20 12:04:24 +0530

    Delete user.rb

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-	has_and_belongs_to_many :roles
-	has_many :admins
-	has_many :sellers
-	has_many :buyers
-	has_many :user_images
-	has_and_belongs_to_many :addresses
-end
